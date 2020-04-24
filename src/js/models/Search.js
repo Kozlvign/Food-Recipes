c11be87a507789aea891d80e859a61a9096e54f1
Kozlvign <63605496+Kozlvign@users.noreply.github.com>
2020-04-24 11:32:06 -0300

    Starting with the search module

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
-async function getResult(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    try {
-      const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-      const recipes = res.data.recipes;
-      console.log(recipes);
-    } catch(error) {
-        alert(error);
+export default class Search {
+    constructor(query) {
+        this.query = query;
     }
+
+    async getResults(query) {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
   }
-  getResult('pizza');
+}
