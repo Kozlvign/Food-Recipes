9825e6d3cffd700d5937463dad5469bfe49db031
Kozlvign <63605496+Kozlvign@users.noreply.github.com>
2020-04-24 11:08:57 -0300

    Starting my project

---
+++ src/js/models/Search.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try {
+      const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+      const recipes = res.data.recipes;
+      console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+  }
+  getResult('pizza');
