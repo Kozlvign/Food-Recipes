90f891ba864d9784101fc0fe4d91e3a9b38b8ad3
Kozlvign <63605496+Kozlvign@users.noreply.github.com>
2020-04-26 05:51:35 -0300

    recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -35,3 +36,8 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+
+const r = new Recipe(46956);
+r.getRecipe();
+console.log(r);
